5caadfd9d0cc9774f6320add851da4fea2c63694
paulinhovc <suellen.azevedo13@gmail.com>
2019-12-19 11:17:28 -0300

    contato

--- projeto-integrador-eco/src/app/app.module.ts
+++ projeto-integrador-eco/src/app/app.module.ts
@@ -6,11 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/static/header/header.component';
 import { FooterComponent } from './components/static/footer/footer.component';
 import { BodyComponent } from './components/static/body/body.component';
-<<<<<<< HEAD
 import { SobreNosComponent } from './components/dynamic/sobre-nos/sobre-nos.component';
-=======
-import { HomeComponent } from './components/dinamic/home/home.component';
->>>>>>> f5591744764b5e60bb95178be3e4762d918e4f75
+import { ContatoComponent } from './components/dynamic/contato/contato.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +15,8 @@ import { HomeComponent } from './components/dinamic/home/home.component';
     HeaderComponent,
     FooterComponent,
     BodyComponent,
-<<<<<<< HEAD
-    SobreNosComponent
-=======
-    HomeComponent
->>>>>>> f5591744764b5e60bb95178be3e4762d918e4f75
+    SobreNosComponent,
+ContatoComponent
   ],
   imports: [
     BrowserModule,
