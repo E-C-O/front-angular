f5591744764b5e60bb95178be3e4762d918e4f75
Turma Generation <samanta.guimaraes@usp.br>
2019-12-19 09:49:59 -0300

    criacao da home

--- projeto-integrador-eco/src/app/app.module.ts
+++ projeto-integrador-eco/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/static/header/header.component';
 import { FooterComponent } from './components/static/footer/footer.component';
 import { BodyComponent } from './components/static/body/body.component';
+import { HomeComponent } from './components/dinamic/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
