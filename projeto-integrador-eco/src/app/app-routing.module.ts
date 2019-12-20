1bac3c8dca3db51964cfe610e4915b80f7e9bda4
Turma Generation <suellen.azevedo13@gmail.com>
2019-12-20 08:30:04 -0300

    Rotas

--- projeto-integrador-eco/src/app/app-routing.module.ts
+++ projeto-integrador-eco/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { HomeComponent } from './components/dynamic/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './components/dynamic/contato/contato.component';
+import { SobreNosComponent } from './components/dynamic/sobre-nos/sobre-nos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'about', component: SobreNosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
